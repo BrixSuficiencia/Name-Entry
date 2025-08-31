587048169dc0231cad59b48eafa265fa6f6bfef3
unknown <brixsuficiencia@gmail.com>
2025-08-31 19:03:14 +0800

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/', 
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
